d7bd9a098bd8e70788fae1dcc027f76728f3d379
Sisoooo <f.sisani2002@gmail.com>
2025-11-01 16:26:13 +0100

    Repo sintonizzata in locale

--- MCM_lab1_2025-26/mcm_assingment1_matlab/include/RotToYPR.m
+++
@@ -1,7 +0,0 @@
-function [psi,theta,phi] = rotToYPR(R)
-% Given a rotation matrix the function outputs the relative euler angles
-% usign the convention YPR
-% Check that R is a valid rotation matrix using IsRotationMatrix().
-
-end
-
