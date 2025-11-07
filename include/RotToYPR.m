8538853688e989efcd045c3dd678905c608be1f5
Sisoooo <f.sisani2002@gmail.com>
2025-11-07 11:55:57 +0100

    Odio Matlab

--- include/RotToYPR.m
+++ include/RotToYPR.m
@@ -8,12 +8,20 @@ function [psi,theta,phi] = RotToYPR(R)
     end
 
     theta = atan2(-R(3,1), sqrt(R(1,1)^2 + R(2,1)^2));
-    if cos(theta) ~= 0
+
+    if abs(cos(theta)) > 1e-6
         psi = atan2(R(2,1), R(1,1));
         phi = atan2(R(3,2), R(3,3));
     else
-        error("Singular configuration");
+        psi = atan2(-R(1,2), R(2,2));
+        phi = 0;
     end
 
+    % Normalize angles to [0, 2*pi)
+    if psi < 0
+        psi = psi + 2*pi;
+    end
+    if phi < 0
+        phi = phi + 2*pi;
+    end
 end
-
